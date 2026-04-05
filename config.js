f2fc3eefad5c3e8b7a47e896f8de0782e6355f42
rauladi <ridwan.auladi@gmail.com>
2026-04-05 13:26:25 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js – DO NOT COMMIT TO GITHUB
 window.API_CONFIG = {
-  GEMINI_API_KEY: "AIzaSy..."  // your actual Gemini key
+  GEMINI_API_KEY: "AIzaSyApvyewv8wnJzxqPbZdmvJBywUrCnaG4Ag"  // your actual Gemini key
 };
